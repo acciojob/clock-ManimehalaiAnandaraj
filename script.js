2e69b3c624a500c9f3de685ac4ac939cfc42738c
ManimehalaiAnandaraj <manimehalaiannakodi@gmail.com>
2025-03-27 10:06:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let timerdisplay=document.getElementById("timer");
+
+let currentdate=new Date();
+
+setInterval(
+	function() {
+		currentdate=new Date();
+		timerdisplay.innerHTML=currentdate.toLocaleString();
+		
+	},1000
+)
